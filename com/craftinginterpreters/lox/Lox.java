010e462aad7f932fe97dfd345f73d2ce83da7919
Danny Nguyen <nguyenduongpa@gmail.com>
2024-03-27 09:54:52 -0700

    changed folder structures

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -1,4 +1,4 @@
-package lox;
+package com.craftinginterpreters.lox;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,6 +9,8 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+
+    static boolean hadError = false;
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -20,20 +22,47 @@ public class Lox {
         }
     }
 
+    // if user input the correct file, run it
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError) System.exit(65);
     }
 
+    // if user don't enter a value initially, run an interactive prompt and ask for an input
     private static void runPrompt() throws IOException {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
 
+        // infinite loop that asks for an input until a null (ctrl + d) is sent in, then it breaks out of the loop
         for (;;) {
             System.out.print("> ");
             String line = reader.readLine();
             if (line == null) break;
             run(line);
+            hadError = false;
         }
+    }
+
+    // the core function
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    // error handling
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println(
+            "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
     }
 }
