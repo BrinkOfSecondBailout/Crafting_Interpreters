d2cab29b6557324abab52d8b2da0f46d76171c85
Danny Nguyen <nguyenduongpa@gmail.com>
2024-04-07 16:38:43 -0700

    done

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -39,7 +39,7 @@ class Parser {
                 return new Expr.Assign(name, value);
             }
 
-            error(equals, "invalid assignment target.");
+            error(equals, "Invalid assignment target.");
         }
 
         return expr;
@@ -59,6 +59,8 @@ class Parser {
     private Stmt statement() {
         if (match(PRINT)) return printStatement();
 
+        if (match(LEFT_BRACE)) return new Stmt.Block(block());
+
         return expressionStatement();
     }
 
@@ -72,6 +74,17 @@ class Parser {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
+    }
+
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after block.");
+        return statements;
     }
 
     private Stmt varDeclaration() {
