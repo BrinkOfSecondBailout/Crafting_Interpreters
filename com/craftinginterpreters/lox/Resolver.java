e4f49ea3818a184ce503e0e812cb9917b7c132fd
Danny Nguyen <nguyenduongpa@gmail.com>
2024-04-11 11:12:44 -0700

    d

--- com/craftinginterpreters/lox/Resolver.java
+++ com/craftinginterpreters/lox/Resolver.java
@@ -5,6 +5,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
-class Resolver {
-    
+class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
+    private final Interpreter interpreter;
+
+    Resolver(Interpreter interpreter) {
+        this.interpreter = interpreter;
+    }
 }
