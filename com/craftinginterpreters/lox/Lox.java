253ac1f821ab38a08dad86fb621d2265892e6598
Danny Nguyen <nguyenduongpa@gmail.com>
2024-04-11 18:44:25 -0700

    d

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -62,6 +62,9 @@ public class Lox {
         Resolver resolver = new Resolver(interpreter);
         resolver.resolve(statements);
 
+        // Stop if there was resolution error
+        if (hadError) return;
+
         interpreter.interpret(statements);
     }
 
