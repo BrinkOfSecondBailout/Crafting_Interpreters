7bae53cf8c8255ee7c597b12e592eb9321f05195
Danny Nguyen <nguyenduongpa@gmail.com>
2024-04-13 14:39:16 -0700

    d

--- com/craftinginterpreters/lox/Resolver.java
+++ com/craftinginterpreters/lox/Resolver.java
@@ -16,7 +16,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
     }
 
     private enum FunctionType {
-        NONE, FUNCTION, METHOD
+        NONE, FUNCTION, METHOD, INITIALIZER
     }
 
     private enum ClassType {
@@ -43,6 +43,9 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
 
         for (Stmt.Function method : stmt.methods) {
             FunctionType declaration = FunctionType.METHOD;
+            if (method.name.lexeme.equals("init")) {
+                declaration = FunctionType.INITIALIZER;
+            }
             resolveFunction(method, declaration);
         }
 
@@ -87,6 +90,9 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void>{
         }
 
         if (stmt.value != null) {
+            if (currentFunction == FunctionType.INITIALIZER) {
+                Lox.error(stmt.keyword, "Can't return a value from an initializer");
+            }
             resolve(stmt.value);
         }
         
