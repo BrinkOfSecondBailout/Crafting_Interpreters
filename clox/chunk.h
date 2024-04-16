b8b79a5bd86c3da0790b7645264e29eee2aa74b1
Danny Nguyen <nguyenduongpa@gmail.com>
2024-04-15 18:08:29 -0700

    d

--- clox/chunk.h
+++ clox/chunk.h
@@ -1,4 +1,4 @@
-#ifdef clox_chunk_h
+#ifndef clox_chunk_h
 #define clox_chunk_h
 
 #include "common.h"
@@ -12,5 +12,9 @@ typedef struct {
     int capacity;
     uint8_t* code;
 } Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
 
 #endif
