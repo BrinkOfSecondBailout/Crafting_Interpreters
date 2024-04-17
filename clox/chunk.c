e8462f2341f07db4f15718b75e7194653d25e5c3
Danny Nguyen <nguyenduongpa@gmail.com>
2024-04-16 18:29:14 -0700

    d

--- clox/chunk.c
+++ clox/chunk.c
@@ -14,8 +14,7 @@ void freeChunk(Chunk* chunk) {
 }
 
 void writeChunk(Chunk *chunk, uint8_t byte) {
-    if (chunk->capacity < chunk->count + 1)
-    {
+    if (chunk->capacity < chunk->count + 1) {
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
--- clox/debug.c
+++ clox/debug.c
@@ -2,15 +2,15 @@
 
 #include "debug.h"
 
-void disassembleChunk(Chunk* chunk, const char* name) {
-    printf("== %s == \n", name);
+void disassembleChunk(Chunk *chunk, const char *name) {
+    printf("== %s ==\n", name);
 
     for (int offset = 0; offset < chunk->count;) {
         offset = disassembleInstruction(chunk, offset);
     }
 }
 
-int disassembleInstruction(Chunk* chunk, int offset) {
+int disassembleInstruction(Chunk *chunk, int offset) {
     printf("%04d ", offset);
 
     uint8_t instruction = chunk->code[offset];
@@ -23,7 +23,7 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     }
 }
 
-static int simpleInstruction(const char* name, int offset) {
+int simpleInstruction(const char* name, int offset) {
     printf("%s\n", name);
     return offset + 1;
 }
