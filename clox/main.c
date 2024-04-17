89991b18d3b69187f3b690abba3bf036919408d5
Danny Nguyen <nguyenduongpa@gmail.com>
2024-04-17 07:54:27 -0700

    d

--- clox/main.c
+++ clox/main.c
@@ -7,8 +7,9 @@ int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
-    
+
     disassembleChunk(&chunk, "test chunk");
+    
     freeChunk(&chunk);
 
     return 0;
