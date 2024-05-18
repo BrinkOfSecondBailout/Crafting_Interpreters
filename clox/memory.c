2c667dc40e67f62b16efe5c7496f957c7c530899
Danny Nguyen <nguyenduongpa@gmail.com>
2024-05-17 17:44:13 -0700

    d

--- clox/memory.c
+++ clox/memory.c
@@ -34,6 +34,9 @@ static void freeObject(Obj* object) {
             FREE(ObjString, object);
             break;
         }
+        case OBJ_UPVALUE:
+            FREE(ObjUpvalue, object);
+            break;
     }
 }
 
