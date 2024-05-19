337cac463b6c8175cc14d16397b67e873cc3cda1
Danny Nguyen <nguyenduongpa@gmail.com>
2024-05-19 12:09:53 -0700

    d

--- clox/debug.c
+++ clox/debug.c
@@ -93,6 +93,8 @@ int disassembleInstruction(Chunk *chunk, int offset) {
             return jumpInstruction("OP_LOOP", -1, chunk, offset);
         case OP_CALL:
             return byteInstruction("OP_CALL", chunk, offset);
+        case OP_INVOKE:
+            return invokeInstruction("OP_INVOKE", chunk, offset);
         case OP_CLOSURE:
             offset++;
             uint8_t constant = chunk->code[offset++];
@@ -128,6 +130,15 @@ int constantInstruction(const char* name, Chunk* chunk, int offset) {
     printValue(chunk->constants.values[constant]);
     printf("'\n");
     return offset + 2;
+}
+
+static int invokeInstruction(const char* name, Chunk* chunk, int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    uint8_t argCount = chunk->code[offset + 2];
+    printf("%-16s (%d args) %4d '", name, argCount, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 3;
 }
 
 int simpleInstruction(const char* name, int offset) {
