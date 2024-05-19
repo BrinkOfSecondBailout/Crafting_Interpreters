b29591c0b4c65232eb14af0b18dd6263816cddd2
Danny Nguyen <nguyenduongpa@gmail.com>
2024-05-19 09:34:14 -0700

    d

--- clox/vm.c
+++ clox/vm.c
@@ -299,6 +299,19 @@ static InterpretResult run() {
                 runtimeError("Undefined property '%s'.", name->chars);
                 return INTERPRET_RUNTIME_ERROR;
             }
+            case OP_SET_PROPERTY: {
+                if (!IS_INSTANCE(peek(1))) {
+                    runtimeError("Only instances have fields.");
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+
+                ObjInstance* instance = AS_INSTANCE(peek(1));
+                tableSet(&instance->fields, READ_STRING(), peek(0));
+                Value value = pop();
+                pop();
+                push(value);
+                break;
+            }
             case OP_EQUAL: {
                 Value b = pop();
                 Value a = pop();
